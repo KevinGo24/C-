42ce652e1167612725f693a87ef4d1e5903df002
kevin <kevin@gmail.com>
2026-07-27 19:32:54 -0500

    se hizo cambio

--- primero_pasos/Program.cs
+++ primero_pasos/Program.cs
@@ -1,1 +1,13 @@
-﻿using primero_pasos.Models;
+﻿using System;
+using primero_pasos.Models;
+
+Paciente paciente1 = new Paciente
+{
+    Id = 1,
+    Nombre = "Carlos Mendoza",
+    Edad = 34,
+    Sintoma = "Fiebre alta"
+};
+
+Console.WriteLine("--- Paciente Registrado ---");
+Console.WriteLine($"[ID: {paciente1.Id}] {paciente1.Nombre} - {paciente1.Edad} años- {paciente1.Sintoma} sintoma");
